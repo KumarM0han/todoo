2da5f390dd71a7d76146b5e8d36f21a6733bd264
Choudhary <kumar-mohan.choudhary@siemens.com>
2025-12-09 21:43:43 +0530

    Shows modal box for editing todo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,7 @@
 #include <QLineEdit>
 #include <QShortcut>
 #include <QPushButton>
+#include <QInputDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -23,20 +24,30 @@ MainWindow::MainWindow(QWidget *parent)
     line->setMaxLength(256);
 
     QPushButton *add_todo_button = new QPushButton("Add", this);
-
-    QStringListModel* todos = new QStringListModel();
+    QStringListModel* todos = new QStringListModel(this);
     QStringList todo;
     todos->setStringList(todo);
     QListView *l = new QListView(this);
     l->setModel(todos);
+    l->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    connect(l, &QAbstractItemView::doubleClicked, this, [=] (const QModelIndex& index) {
+        QString current = index.data(Qt::DisplayRole).toString();
+        bool ok{};
+        QString updated_todo = QInputDialog::getText(this, "Edit Todo", "Enter new todo", QLineEdit::Normal, current, &ok);
+        if (ok && !updated_todo.isEmpty()) {
+            todos->setData(index, updated_todo);
+        }
+    });
 
     connect(add_todo_button, &QPushButton::clicked, this, [=] () {
         if (line->hasAcceptableInput()) {
             QString entered_line = line->text();
-            line->clear();
-            int last = todos->rowCount();
-            todos->insertRow(last);
-            todos->setData(todos->index(last), entered_line);
+            if (entered_line.isEmpty() == false) {
+                line->clear();
+                int last = todos->rowCount();
+                todos->insertRow(last);
+                todos->setData(todos->index(last), entered_line);
+            }
         }
     });
 
